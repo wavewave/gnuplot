3e491c289d27f9df1f6fe13d33b09e0ff626b6ce
haskell <haskell@henning-thielemann.de>
2009-08-05 20:37:37 +0000

    Graph.TwoDimensional

---
+++ src/Graphics/Gnuplot/Graph/TwoDimensional.hs
@@ -0,0 +1,38 @@
+module Graphics.Gnuplot.Graph.TwoDimensional (
+   T,
+   Type,
+
+   defaultType,
+   deflt,
+
+   typ,
+   lineSpec,
+
+   lines,
+   points,
+   linesPoints,
+   impulses,
+   dots,
+   steps,
+   fSteps,
+   hiSteps,
+   errorBars,
+   xErrorBars,
+   yErrorBars,
+   xyErrorBars,
+   errorLines,
+   xErrorLines,
+   yErrorLines,
+   xyErrorLines,
+   boxes,
+   filledCurves,
+   boxErrorBars,
+   boxXYErrorBars,
+   financeBars,
+   candleSticks,
+   vectors,
+   ) where
+
+import Graphics.Gnuplot.Private.Graph2D
+
+import Prelude hiding (lines, )
