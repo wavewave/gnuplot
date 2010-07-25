795c0533c5dad3ca054a6b8e6ea6a1ec3201768a
haskell <haskell@henning-thielemann.de>
2010-07-25 10:44:34 +0000

    Frame.Option: comment on non-working suppression of exporting Cons

--- src/Graphics/Gnuplot/Frame/Option.hs
+++ src/Graphics/Gnuplot/Frame/Option.hs
@@ -4,5 +4,6 @@ module Graphics.Gnuplot.Frame.Option (
    ) where
 
 -- exclude Cons constructor
+-- this does not work, Cons is exported anyway
 import Graphics.Gnuplot.Private.FrameOption hiding (T)
 import qualified Graphics.Gnuplot.Private.FrameOption as Option
