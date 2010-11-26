40323ad1cbcd5e1c98a07dae7a0b5465321540a3
haskell <haskell@henning-thielemann.de>
2010-11-26 10:02:32 +0000

    Terminal.PostScript.font, embedFont

--- src/Graphics/Gnuplot/Terminal/PostScript.hs
+++ src/Graphics/Gnuplot/Terminal/PostScript.hs
@@ -2,18 +2,21 @@ module Graphics.Gnuplot.Terminal.PostScript (
    T, cons,
    landscape, portrait, eps,
    color, monochrome,
+   font, embedFont,
    ) where
 
 import qualified Graphics.Gnuplot.Terminal as Terminal
-import Data.Maybe (catMaybes, )
 import Graphics.Gnuplot.Utility (quote, )
+import Data.Maybe (maybeToList, )
 
 
 data T =
    Cons {
       filename_ :: FilePath,
       mode_ :: Maybe Mode,
-      color_ :: Maybe Bool
+      color_ :: Maybe Bool,
+      embedFont_ :: [FilePath],
+      font_ :: Maybe (String, Int)
    }
 
 cons :: FilePath -> T
@@ -21,7 +24,9 @@ cons path =
    Cons {
       filename_ = path,
       mode_ = Nothing,
-      color_ = Nothing
+      color_ = Nothing,
+      embedFont_ = [],
+      font_ = Nothing
    }
 
 
@@ -42,6 +47,19 @@ color term =
 monochrome :: T -> T
 monochrome term =
    term{color_ = Just False}
+
+font :: String -> Int -> T -> T
+font fontName fontSize term =
+   term{font_ = Just (fontName, fontSize)}
+
+{- |
+Embed a font file in the generated PostScript output.
+Each call adds a new font file,
+there is no way to remove it again.
+-}
+embedFont :: FilePath -> T -> T
+embedFont fontFile term =
+   term{embedFont_ = fontFile : embedFont_ term}
 
 
 -- private functions
@@ -67,10 +85,11 @@ instance Terminal.C T where
       Terminal.Cons {
          Terminal.options =
             "postscript" :
-            catMaybes (
-               (fmap formatMode $ mode_ term) :
-               (fmap (\b -> if b then "color" else "monochrome") $ color_ term) :
-               []),
+            (maybeToList $ fmap formatMode $ mode_ term) ++
+            (maybeToList $ fmap (\b -> if b then "color" else "monochrome") $ color_ term) ++
+            (concatMap (\path -> "fontfile" : quote path : []) $ embedFont_ term) ++
+            (maybe [] (\(name,size) -> "font" : quote name : show size : []) $ font_ term) ++
+            [],
          Terminal.commands =
             ["set output " ++ (quote $ filename_ term)]
       }
